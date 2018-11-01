5e735ca17d30af1884dcdcf10664ad7a3a190afc
amir.hussain <amir.hussain@mobigesture.com>
2018-11-01 12:17:26 -0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+export const RoutingComponents = [
+  HomeComponent
+];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
